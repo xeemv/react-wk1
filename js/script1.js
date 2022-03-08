492cae0e7d6f2bbca0a92ff2c85c655a7476d8f4
xeemv <xeeyang08@gmail.com>
2022-03-07 23:20:05 -0600

    new wk files

---
+++ js/script1.js
@@ -0,0 +1,15 @@
+// first video - react
+/**
+ * A js library for building user interfaces
+		○ Written and maintained by fb
+		○ Consider the best option for writing the front-end user interfaces
+* Allows us to easily organize our user interface into individual components that we can then use where ever the UI needs them
+    - think of it like creating our own html elements w/ custom functionality 
+    - then using those elements else where
+* the components can be just about anything but they tend to be organized by visual logic segments
+    - i.e: specific widgets we'd see on any site
+* another valuable feature is:
+    - the virtual dom used w/ js or jquery to access and manipulate the dom
+* IMPORTANT TO NOTE:
+    - WHEN WE MESS W/ THE DOM DIRECTLY, WE DO IT AT THE COST OF THE APPLICATION EFFICENCY 
+ */
